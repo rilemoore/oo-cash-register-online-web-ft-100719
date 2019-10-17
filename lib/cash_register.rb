e45c85d788ab0e2f78985e8c1076acb386c23756
Riley Moore <rilemoore@gmail.com>
2019-10-17 15:23:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
   def apply_discount()
     if(discount != 0)
       @total = @total - (@total * (@discount/100.00))
-      "After the discount, the total comes to $#{@total.to_i}"
+      "After the discount, the total comes to $#{@total.to_i}."
     else
       "There is no discount to apply."
     end
